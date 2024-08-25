a4b02d5768a9e374867e485694d6ae5b41e7ab33
thrashdev <thrashdev@protonmail.com>
2024-08-25 10:35:34 +0500

    added metrics, TODO:check why doesn't increment past 1

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 )
 
 type apiConfig struct {
 	fileServerHits int
 }
 
-func (cfg *apiConfig) middlewareMetricsInt(next http.Handler) http.Handler {
-
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileServerHits++
+	return next
 }
 
-func handleReadiness(w http.ResponseWriter, r *http.Request) {
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
 	w.Header().Set("Content-Type", "text-plain")
@@ -20,10 +22,18 @@ func handleReadiness(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	hits := "Hits: " + strconv.Itoa(cfg.fileServerHits)
+	w.WriteHeader(200)
+	w.Write([]byte(hits))
+}
+
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	serveMux.HandleFunc("/healthz/", handleReadiness)
+	apiConf := apiConfig{}
+	serveMux.Handle("/app/", http.StripPrefix("/app/", apiConf.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	serveMux.HandleFunc("/healthz/", handlerReadiness)
+	serveMux.HandleFunc("/metrics/", apiConf.handlerMetrics)
 	server := http.Server{Handler: serveMux, Addr: ":8080"}
 	server.ListenAndServe()
 }
