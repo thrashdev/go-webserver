0922e9b6cdb629fe27d82c22af1e79e015316662
Danil Samsonov <thrashdev@protonmail.com>
2024-08-26 17:46:25 +0500

    started working on length validation

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 )
@@ -36,13 +38,48 @@ func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request
 	cfg.fileServerHits = 0
 }
 
+func (cfg *apiConfig) handlerAdminMetrics(w http.ResponseWriter, r *http.Request) {
+	template_html := `<html>
+
+<body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+</body>
+
+</html>`
+
+	res := fmt.Sprintf(template_html, cfg.fileServerHits)
+	w.Write([]byte(res))
+	w.WriteHeader(200)
+	w.Header().Add("Content-Type", "text/html")
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		fmt.Println("AAAAAAAH COULDN'T DECORE JSOOOOOOOON")
+
+	}
+	if len(params.body) > 140 {
+
+	}
+}
+
 func main() {
 	serveMux := http.NewServeMux()
 	apiConf := apiConfig{}
 	serveMux.Handle("/app/", http.StripPrefix("/app/", apiConf.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
-	serveMux.HandleFunc("/healthz/", handlerReadiness)
-	serveMux.HandleFunc("/metrics/", apiConf.handlerMetrics)
-	serveMux.HandleFunc("/reset/", apiConf.handlerResetMetrics)
+	serveMux.HandleFunc("GET /api/healthz/", handlerReadiness)
+	serveMux.HandleFunc("GET /api/metrics/", apiConf.handlerMetrics)
+	serveMux.HandleFunc("/api/reset/", apiConf.handlerResetMetrics)
+	serveMux.HandleFunc("/admin/metrics", apiConf.handlerAdminMetrics)
+	serveMux.HandleFunc("api/validate_chirp", handlerValidateChirp)
 	server := http.Server{Handler: serveMux, Addr: ":8080"}
 	server.ListenAndServe()
 }
